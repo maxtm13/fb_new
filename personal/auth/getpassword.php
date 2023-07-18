8caaebda43d44fb7bc5a635f939a557878f819cb
maxtm13 <maxtm1@yandex.ru>
2023-07-18 09:16:04 +0300

    Сделал слайдер и корзину > Персональный раздел, запрос пароля. Обновление директорий

---
+++ personal/auth/getpassword.php
@@ -0,0 +1,16 @@
+<?
+define("NEED_AUTH", true);
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords_inner", "Восстановление пароля");
+$APPLICATION->SetPageProperty("title", "Восстановление пароля");
+$APPLICATION->SetPageProperty("keywords", "Восстановление пароля");
+$APPLICATION->SetPageProperty("description", "Восстановление пароля");
+$APPLICATION->SetTitle("Восстановление пароля");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.auth.forgotpasswd",
+	"flat",
+	Array(
+		"AUTH_AUTH_URL" => "index.php",
+		"AUTH_REGISTER_URL" => "registration.php"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
