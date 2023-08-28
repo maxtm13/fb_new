6416643d374410641ebbb23061ea8e2c0b9bc980
maxtm13 <maxtm1@yandex.ru>
2023-08-28 19:18:15 +0300

    Мобильная адаптация главной стр

--- local/templates/fashionbrick/components/bitrix/sale.basket.basket.line/top_menu_basket/top_template.php
+++ local/templates/fashionbrick/components/bitrix/sale.basket.basket.line/top_menu_basket/top_template.php
@@ -22,26 +22,26 @@ $compositeStub = (isset($arResult['COMPOSITE_STUB']) && $arResult['COMPOSITE_STU
                 &nbsp;
                 <a href="?logout=yes&<?= bitrix_sessid_get() ?>"><?= GetMessage('TSB1_LOGOUT') ?></a>
             <? else:
-                $arParamsToDelete = array(
-                    "login",
-                    "login_form",
-                    "logout",
-                    "register",
-                    "forgot_password",
-                    "change_password",
-                    "confirm_registration",
-                    "confirm_code",
-                    "confirm_user_id",
-                    "logout_butt",
-                    "auth_service_id",
-                    "clear_cache",
-                    "backurl",
-                );
+            $arParamsToDelete = array(
+                "login",
+                "login_form",
+                "logout",
+                "register",
+                "forgot_password",
+                "change_password",
+                "confirm_registration",
+                "confirm_code",
+                "confirm_user_id",
+                "logout_butt",
+                "auth_service_id",
+                "clear_cache",
+                "backurl",
+            );
 
-                $currentUrl = urlencode($APPLICATION->GetCurPageParam("", $arParamsToDelete));
+            $currentUrl = urlencode($APPLICATION->GetCurPageParam("", $arParamsToDelete));
             if ($arParams['AJAX'] == 'N')
             {
-                ?>
+            ?>
                 <script type="text/javascript"><?=$cartId?>.currentUrl = '<?=$currentUrl?>';</script><?
             }
             else {
@@ -71,48 +71,48 @@ $compositeStub = (isset($arResult['COMPOSITE_STUB']) && $arResult['COMPOSITE_STU
             <? endif ?>
         </div>
     <? endif ?>
-<!--    <a class="cart__link" href="--><?//= $arParams['PATH_TO_BASKET'] ?><!--">111-->
+    <!--    <a class="cart__link" href="--><? //= $arParams['PATH_TO_BASKET'] ?><!--">111-->
     <a href="<?= $arParams['PATH_TO_BASKET'] ?>">
-    <div class="bx-basket-block">
-        <?
-        if (!$arResult["DISABLE_USE_BASKET"]) {
+        <div class="bx-basket-block">
+            <?
+            if (!$arResult["DISABLE_USE_BASKET"]) {
             ?><i class="img-cart">
-                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="26" viewBox="0 0 23 26" stroke="#f5f5f5"
-                     fill="none">
-                    <path d="M2.02421 9.01692C2.07258 8.415 2.57517 7.95117 3.17904 7.95117H19.2878C19.8917 7.95117 20.3943 8.415 20.4426 9.01692L21.4592 21.6681C21.5676 23.0167 20.5025 24.1708 19.1495 24.1708H3.31724C1.96437 24.1708 0.899225 23.0167 1.0076 21.6681L2.02421 9.01692Z"
-                          stroke="#f5f5f5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M15.8675 11.4269V5.63419C15.8675 3.0748 13.7927 1 11.2333 1C8.67392 1 6.59912 3.0748 6.59912 5.63419V11.4269"
-                          stroke="#f5f5f5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.02421 10.0169C4.07258 9.415 4.57517 8.95117 5.17904 8.95117H21.2878C21.8917 8.95117 22.3943 9.415 22.4426 10.0169L23.4592 22.6681C23.5676 24.0167 22.5025 25.1708 21.1495 25.1708H5.31724C3.96437 25.1708 2.89922 24.0167 3.0076 22.6681L4.02421 10.0169Z"
+                          stroke="#F5F5F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M17.8677 12.4269V6.63419C17.8677 4.0748 15.7928 2 13.2335 2C10.6741 2 8.5993 4.0748 8.5993 6.63419V12.4269"
+                          stroke="#F5F5F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
             </i>
             <div class="basket_name"><?= GetMessage('TSB1_CART') ?><?
-        }
+                }
 
-        if (!$compositeStub) {
-            if ($arParams['SHOW_NUM_PRODUCTS'] == 'Y' && ($arResult['NUM_PRODUCTS'] > 0 || $arParams['SHOW_EMPTY_VALUES'] == 'Y')) {
-                echo (' ('.$arResult['NUM_PRODUCTS'].')');?>
+                if (!$compositeStub) {
+                    if ($arParams['SHOW_NUM_PRODUCTS'] == 'Y' && ($arResult['NUM_PRODUCTS'] > 0 || $arParams['SHOW_EMPTY_VALUES'] == 'Y')) {
+                        echo(' (' . $arResult['NUM_PRODUCTS'] . ')'); ?>
 
-<?
-                if ($arParams['SHOW_TOTAL_PRICE'] == 'Y') {
-                    ?>
-                    <br <? if ($arParams['POSITION_FIXED'] == 'Y'): ?>class="hidden-xs"<? endif; ?>/>
-                    <span>
+                        <?
+                        if ($arParams['SHOW_TOTAL_PRICE'] == 'Y') {
+                            ?>
+                            <br <? if ($arParams['POSITION_FIXED'] == 'Y'): ?>class="hidden-xs"<? endif; ?>/>
+                            <span>
 						<?= GetMessage('TSB1_TOTAL_PRICE') ?> <strong><?= $arResult['TOTAL_PRICE'] ?></strong>
 					</span>
-                    <?
+                            <?
+                        }
+                    }
                 }
-            }
-        }
 
-        if ($arParams['SHOW_PERSONAL_LINK'] == 'Y'):?>
-            <div style="padding-top: 4px;">
-                <span class="icon_info"></span>
-                <a href="<?= $arParams['PATH_TO_PERSONAL'] ?>"><?= GetMessage('TSB1_PERSONAL') ?></a>
+                if ($arParams['SHOW_PERSONAL_LINK'] == 'Y'):?>
+                    <div style="padding-top: 4px;">
+                        <span class="icon_info"></span>
+                        <a href="<?= $arParams['PATH_TO_PERSONAL'] ?>"><?= GetMessage('TSB1_PERSONAL') ?></a>
+                    </div>
+                <? endif ?>
+
             </div>
-        <? endif ?>
 
-    </div>
-
-<!--    </a>-->
-</div>
+            <!--    </a>-->
+        </div>
     </a>
+</div>
--- local/templates/fashionbrick/js/myscripts.js
+++ local/templates/fashionbrick/js/myscripts.js
@@ -1,89 +1,83 @@
 
-$().fancybox({
-    selector: '[data-fancybox="images"]',
-    thumbs: {
-        // Показывать превью
-        autoStart: false,
-    },
-    keyboard: true,
-    buttons: [
-        // Показывать кнопку закрытия модалки
-        "close"
-    ],
-    // Эффект анимации при перелючении
-    transitionEffect: "zoom-in-out",
-    // Зацикливать превью
-    loop: true,
-});
 
-$(document).ready(function(){
-   $('.slider__main-inner').slick({
-       dots: true,
-       infinite: true,
-       autoplay: true,
-       autoplaySpeed: 5000,
-       //fade:true,
-       // cssEase:'ease',
-       pauseOnFocus: false,
-       pauseOnHover: false
-});   $('.slider__test').slick({
-       dots: true,
-       infinite: true,
-       autoplay: true,
-       autoplaySpeed: 5000,
-       //fade:true,
-       // cssEase:'ease',
-       pauseOnFocus: false,
-       pauseOnHover: false
-});
-});
-// $(document).ready(function() {
-//     $('.new_slider').slick({
-//         // dots: true,
-//         // infinite: false,
-//         // autoplay: true,
-//         // autoplaySpeed: 2000
-//     });
-// });
-// $(document).ready(function() {
-//     $('.bx_1847241719_21').slick({
-//         // dots: true,
-//         // infinite: false,
-//          autoplay: true,
-//         // autoplaySpeed: 2000
-//     });
-// })
-
-$(document).ready(function(){
+$(document).ready(function () {
+    //Слайдер баннеров
+    $('.slider__main-inner').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        //fade:true,
+        // cssEase:'ease',
+        pauseOnFocus: false,
+        pauseOnHover: false
+    });
+    // Слайдер разделов каталога
     $('.bx_catalog_line_ul').slick({
         dots: false,
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false,
         infinite: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         //fade:true,
         // cssEase:'ease',
         pauseOnFocus: false,
-        pauseOnHover: false
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
-    // var acc = document.getElementsByClassName("subSection");
-    // var i;
-    //
-    // for (i = 0; i < acc.length; i++) {
-    //     acc[i].addEventListener("click", function() {
-    //         /* Toggle between adding and removing the "active" class,
-    //         to highlight the button that controls the panel */
-    //         this.classList.toggle("active");
-    //
-    //         /* Toggle between hiding and showing the active panel */
-    //         var panel = this.nextElementSibling;
-    //         if (panel.style.display === "block") {
-    //             panel.style.display = "none";
-    //         } else {
-    //             panel.style.display = "block";
-    //         }
-    //     });
-    // }
-});;
+    // Галерея фото
+    $().fancybox({
+        selector: '[data-fancybox="images"]',
+        thumbs: {
+            // Показывать превью
+            autoStart: false,
+        },
+        keyboard: true,
+        buttons: [
+            // Показывать кнопку закрытия модалки
+            "close"
+        ],
+        // Эффект анимации при перелючении
+        transitionEffect: "zoom-in-out",
+        // Зацикливать превью
+        loop: true,
+    });
+    let burger=document.querySelector('.burger-menu')
+    burger.addEventListener("click", function (){
+        document.querySelector('.logo').classList.toggle('active')
+        burger.classList.toggle('active')
+    })
+});
+
+$(document).ready(function () {
+
+
+});
